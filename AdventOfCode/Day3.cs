e4bf6dce40e7987081fbd44007f249915d7ec1e1
Bart Broekman <ROB6035@robeco.nl>
2021-12-02 09:57:14 +0100

    Add day 3 and a day template to the main CS

---
+++ AdventOfCode/Day3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public static class Day3
+    {
+        public static void Part1()
+        {
+
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,8 +6,33 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day1.Part2();
+            //Day1.Part2();
             //Day2.Part2();
+            Day3.Part1();
         }
     }
 }
+
+#region DayTemplate 
+/*
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public static class DayX
+    {
+        public static void Part1()
+        {
+
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
+ */
+#endregion
