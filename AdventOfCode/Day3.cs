6750df1ad4fff36c235602481317e0eab370b960
Bart Broekman <ROB6035@robeco.nl>
2021-12-03 10:00:15 +0100

    Update day 3

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -32,30 +32,27 @@ namespace AdventOfCode
         {
             List<string> bits = new List<string>();
             string input;
-            int inputLength;
 
             while ((input = Console.ReadLine()) != "")
             {
                 bits.Add(input);
             }
-            inputLength = bits[0].Length;
 
-            int oxigen = 0, co2 = 0, i = 0;
+            int i = 0;
             List<string> o2bits = new List<string>(bits), co2bits = new List<string>(bits);
             while (o2bits.Count > 1)
             {
-                o2bits = GetMostCommonBit(o2bits, i);
+                o2bits = GetMostLeastCommonBit(o2bits, i, '1', '0');
                 i++;
             }
             i = 0;
             while (co2bits.Count > 1)
             {
-                co2bits = GetLeastCommonBit(co2bits, i);
+                co2bits = GetMostLeastCommonBit(co2bits, i, '0', '1');
                 i++;
             }
 
-            oxigen = ConvertToBits(o2bits.First());
-            co2 = ConvertToBits(co2bits.First());
+            int oxigen = ConvertToBits(o2bits.First()), co2 = ConvertToBits(co2bits.First());
 
             Console.WriteLine($"Oxigen = {oxigen}; CO2 = {co2}; Life Support = {oxigen * co2}");
         }
@@ -69,24 +66,14 @@ namespace AdventOfCode
             else return 0;
         }
 
-        private static List<string> GetMostCommonBit(List<string> bits, int pos)
+        private static List<string> GetMostLeastCommonBit(List<string> bits, int pos, char most, char least)
         {
             var newBits = bits.Select(s => s.ElementAt(pos).ToString())
                 .Select(c => int.Parse(c));
             if (newBits.Sum() >= (newBits.Count() / 2.0f))
-                return bits.Where(s => s.ElementAt(pos) == '1').ToList();
+                return bits.Where(s => s.ElementAt(pos) == most).ToList();
 
-            else return bits.Where(s => s.ElementAt(pos) == '0').ToList(); 
-        }
-
-        private static List<string> GetLeastCommonBit(List<string> bits, int pos)
-        {
-            var newBits = bits.Select(s => s.ElementAt(pos).ToString())
-                .Select(c => int.Parse(c));
-            if (newBits.Sum() >= (newBits.Count() / 2.0f))
-                return bits.Where(s => s.ElementAt(pos) == '0').ToList();
-
-            else return bits.Where(s => s.ElementAt(pos) == '1').ToList(); 
+            else return bits.Where(s => s.ElementAt(pos) == least).ToList(); 
         }
 
         private static int ConvertToBits(string b)
