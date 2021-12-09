e80941b9b009d48d1260e3d1ee286f040c9dd1ee
Bart Broekman <ROB6035@robeco.nl>
2021-12-09 12:42:40 +0100

    cleanup day 9 more

--- AdventOfCode/Day09.cs
+++ AdventOfCode/Day09.cs
@@ -7,11 +7,11 @@ namespace AdventOfCode
 {
     public static class Day09
     {
-        public static List<(int, int)> CheckedCoords;
+        private static List<(int, int)> CheckedCoords = new();
         public static void Part1()
         {
             string input;
-            List<List<int>> coords = new List<List<int>>();
+            List<List<int>> coords = new();
             while ((input = Console.ReadLine()) != "")
             {
                 coords.Add(new List<int>());
@@ -24,74 +24,13 @@ namespace AdventOfCode
                 for (int j = 0; j < coords.ElementAt(i).Count; j++)
                 {
                     var elem = coords.ElementAt(i).ElementAt(j);
-                    if (i == 0)
-                    {
-                        if (j == 0)
-                        {
-                            if (elem < coords.ElementAt(i).ElementAt(j + 1) && elem < coords.ElementAt(i + 1).ElementAt(j))
-                            {
-                                riskLevel += elem + 1;
-                            }
-                        }
-
-                        else if (j == coords.ElementAt(i).Count - 1)
-                        {
-                            if (elem < coords.ElementAt(i).ElementAt(j - 1) && elem < coords.ElementAt(i + 1).ElementAt(j))
-                            {
-                                riskLevel += elem + 1;
-                            }
-                        }
-
-                        else if (elem < coords.ElementAt(i).ElementAt(j + 1) && elem < coords.ElementAt(i + 1).ElementAt(j) && elem < coords.ElementAt(i).ElementAt(j - 1))
-                        {
-                            riskLevel += elem + 1;
-                        }
-                    }
-
-                    else if (i == coords.Count - 1)
-                    {
-                        if (j == 0)
-                        {
-                            if (elem < coords.ElementAt(i).ElementAt(j + 1) && elem < coords.ElementAt(i - 1).ElementAt(j))
-                            {
-                                riskLevel += elem + 1;
-                            }
-                        }
-
-                        else if (j == coords.ElementAt(i).Count - 1)
-                        {
-                            if (elem < coords.ElementAt(i).ElementAt(j - 1) && elem < coords.ElementAt(i - 1).ElementAt(j))
-                            {
-                                riskLevel += elem + 1;
-                            }
-                        }
-
-                        else if (elem < coords.ElementAt(i).ElementAt(j + 1) && elem < coords.ElementAt(i - 1).ElementAt(j) && elem < coords.ElementAt(i).ElementAt(j - 1))
-                        {
-                            riskLevel += elem + 1;
-                        }
-                    }
-
-                    else if (j == 0)
-                    {
-                        if (elem < coords.ElementAt(i).ElementAt(j + 1) && elem < coords.ElementAt(i + 1).ElementAt(j) && elem < coords.ElementAt(i - 1).ElementAt(j))
-                        {
-                            riskLevel += elem + 1;
-                        }
-                    }
-
-                    else if (j == coords.ElementAt(i).Count - 1)
-                    {
-                        if (elem < coords.ElementAt(i).ElementAt(j - 1) && elem < coords.ElementAt(i + 1).ElementAt(j) && elem < coords.ElementAt(i - 1).ElementAt(j))
-                        {
-                            riskLevel += elem + 1;
-                        }
-                    }
-
-                    else if (elem < coords.ElementAt(i).ElementAt(j + 1) && elem < coords.ElementAt(i + 1).ElementAt(j) && elem < coords.ElementAt(i).ElementAt(j - 1) && elem < coords.ElementAt(i - 1).ElementAt(j))
-                    {
+                    var neighbour = new List<int>();
+                    try { neighbour.Add(coords.ElementAt(i - 1).ElementAt(j)); } catch {}
+                    try { neighbour.Add(coords.ElementAt(i + 1).ElementAt(j)); } catch {}
+                    try { neighbour.Add(coords.ElementAt(i).ElementAt(j - 1)); } catch {}
+                    try { neighbour.Add(coords.ElementAt(i).ElementAt(j + 1)); } catch {}
+                    if (elem < neighbour.Min())
                         riskLevel += elem + 1;
-                    }
                 }
 
             Console.WriteLine(riskLevel);
@@ -108,7 +47,6 @@ namespace AdventOfCode
                     coords.Last().Add(int.Parse(input[i].ToString()));
             }
 
-            CheckedCoords = new List<(int, int)>();
             List<int> riskLevel = new();
 
             for (int i = 0; i < coords.Count; i++)
@@ -119,6 +57,7 @@ namespace AdventOfCode
                         continue;
                     riskLevel.Add(FindBasin(coords, (i, j)));
                 }
+
             List<int> top3 = new();
             for (int i = 0; i < 3; i++)
             {
