d94c5df15b3e5a7331d676bd25f288305144f060
Bart Broekman <broekman@live.nl>
2021-12-15 09:08:33 +0100

    one step firther

--- AdventOfCode/Day15.cs
+++ AdventOfCode/Day15.cs
@@ -7,6 +7,9 @@ namespace AdventOfCode
 {
     public static class Day15
     {
+        private static List<Node> Grid = new();
+        private static List<Node> Unvisited;
+        private static List<Node> Close = new();
         public static void Part1()
         {
             string input;
@@ -15,14 +18,25 @@ namespace AdventOfCode
             {
                 grid.Add(new List<int>(input.ToList().Select(c => int.Parse(c.ToString()))));
             }
-            long risk = 0;
-            List<long> risks = new();
-            Stack<(int, int)> path = new();
-            path.Push((0, 0));
-            var h = FindPath(grid, (0, 0), path);
+            for (int i = 0; i < grid.Count; i++)
+                for (int j = 0; j < grid[i].Count; j++)
+                {
+                    if(i == 0 && j == 0)
+                        Grid.Add(new Node(i, j, 0));
+                    else 
+                    Grid.Add(new Node(i, j, grid[i][j]));
+                }
 
-            foreach ((int x, int y) in path)
-                risk += grid[x][y];
+            Unvisited = new List<Node>(Grid);
+            Unvisited.Sort(new NodeComparer());
+
+            bool finished = false;
+            while (Unvisited.Count > 0 && !finished)
+            {
+                Node curNode =
+            }
+            
+            //var h = FindPath((0, 0), path);
             Console.WriteLine(risk);
         }
 
@@ -31,7 +45,12 @@ namespace AdventOfCode
 
         }*/
 
-        private static int FindPath(List<List<int>> grid, (int, int) curPos, Stack<(int, int)> curPath)
+        private static int FindPath((int, int) curPos, Stack<(int, int)> curPath)
+        {
+
+        }
+
+        /*private static int FindPath((int, int) curPos, Stack<(int, int)> curPath)
         {
             int paths = 0;
             curPath.Push(curPos);
@@ -51,65 +70,29 @@ namespace AdventOfCode
 
             curPath.Pop();
             return paths;
-        }
-
-        /*private static List<long> FindPath(List<List<int>> grid, (int, int) curPos, int risk, List<(int, int)> path)
-        {
-            List<long> paths = new();
-            if (curPos == (grid.Count, grid[0].Count))
-                return new List<long>(risk + grid.Last().Last());
-
-            (int, int)[] positions = new[] { (curPos.Item1 - 1, curPos.Item2), (curPos.Item1, curPos.Item2 - 1), (curPos.Item1 + 1, curPos.Item2), (curPos.Item1, curPos.Item2 + 1) };
-            for (int i = 0; i < positions.Length; i++)
-            {
-                if (!path.Contains(positions[i]))
-                {
-                    try
-                    {
-                        var loc = grid[positions[i].Item1][positions[i].Item2];
-                        path.Add(positions[i]);
-                        paths = paths.Concat(FindPath(grid, positions[i], risk + loc, path)).ToList();
-                    }
-                    catch { continue; };
-                }
-            }
-
-            return paths;
         }*/
 
-        //private static List<(int, int)> FindPath(List<List<int>> grid, (int, int) curPos, List<(int, int)> path)
-        //{
-        //    if (curPos == (grid.Count, grid[0].Count))
-        //        return path;
 
-        //    List<(int, int)> lowestNs = new();
-        //    int cur = grid[curPos.Item1][curPos.Item2];
-        //    (int, int)[] positions = new[] { (curPos.Item1 - 1, curPos.Item2), (curPos.Item1, curPos.Item2 - 1), (curPos.Item1 + 1, curPos.Item2), (curPos.Item1, curPos.Item2 + 1) };
-        //    for (int i = 0; i < positions.Length; i++)
-        //    {
-        //        try
-        //        {
-        //            var n = grid[positions[i].Item1][positions[i].Item2];
-        //            if (n <= cur && !path.Contains(positions[i]))
-        //            {
-        //                if (n < grid[lowestNs[0].Item1][lowestNs[0].Item2])
-        //                    lowestNs.Clear();
-        //                lowestNs.Add(positions[i]);
-        //            }
+    }
+    
+    public class Node
+    {
+        public int x, y, cost, distance = int.MaxValue;
+        public Node previous = null;
+        public bool visited = false;
+        public Node(int x, int y, int cost)
+        {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
+    }
 
-        //        }
-        //        catch { continue; };
-        //    }
-
-        //    foreach (var n in lowestNs)
-        //    {
-        //        var newPath = FindPath(grid, n, path);
-        //        if (newPath == null)
-        //            continue;
-        //        path = path.Concat(newPath).ToList();
-        //    }
-
-        //    return path;
-        //}
+    class NodeComparer : IComparer<Node>
+    {
+        public int Compare(Node n1, Node n2)
+        {
+            return n1.cost.CompareTo(n2.cost);
+        }
     }
 }
