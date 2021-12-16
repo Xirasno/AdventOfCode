a2271f0b320fa9549c85dcd013261343f0db3601
Bart Broekman <broekman@live.nl>
2021-12-16 08:07:25 +0100

    Start day 16

--- AdventOfCode/Day16.cs
+++ AdventOfCode/Day16.cs
@@ -7,14 +7,90 @@ namespace AdventOfCode
 {
     public static class Day16
     {
+        private static Dictionary<char, string> Map = new Dictionary<char, string>()
+        {
+            {'0', "0000"},
+            {'1', "0001"},
+            {'2', "0010"},
+            {'3', "0011"},
+            {'4', "0100"},
+            {'5', "0101"},
+            {'6', "0110"},
+            {'7', "0111"},
+            {'8', "1000"},
+            {'9', "1001"},
+            {'A', "1010"},
+            {'B', "1011"},
+            {'C', "1100"},
+            {'D', "1101"},
+            {'E', "1110"},
+            {'F', "1111"},
+        };
+
         public static void Part1()
         {
+            string packet = Console.ReadLine();
+            string bitOutput = "";
+            for (int i = 0; i < packet.Length; i++)
+                bitOutput += Map[packet[i]];
 
+            List<int> versions = new();
+            List<string> packets = new() { bitOutput };
+            while (packets.Any())
+            {
+                var curPacket = packets.First();
+                if (curPacket.StartsWith("LIT"))
+                    continue;
+                versions.Add(Convert.ToInt32(curPacket[..3], 2));
+                packets = packets.Concat(Unpack(bitOutput)).ToList();
+                packets.Remove(curPacket);
+            }
+
+
+            Console.WriteLine(versions.Aggregate((a, b) => a + b));
         }
 
         /*public static void Part2()
         {
 
         }*/
+
+        private static List<string> Unpack(string bits)
+        {
+            List<string> packets = new();
+            string packet = "";
+            int type = Convert.ToInt32(bits.Substring(3, 3), 2);
+
+            if (type == 4)
+            {
+                packet = "LIT";
+                for (int i = 6; i < bits.Length; i += 5)
+                {
+                    packet += bits.Substring(i + 1, 4);
+                    if (bits[i] == '0')
+                        break;
+                }
+                packets.Add(packet);
+            }
+            else if (type == 6)
+            {
+                char indicator = bits[6];
+                if (indicator == '0')
+                {
+                    int lenght = Convert.ToInt32(bits.Substring(7, 15), 2);
+                    packets = packets.Concat(Unpack(bits.Substring(22, lenght))).ToList();
+                }
+                else if (indicator == '1')
+                {
+                    int nr = Convert.ToInt32(bits.Substring(7, 11), 2);
+                    for (int i = 0; i < nr; i++)
+                    {
+                    }
+                }
+
+            }                
+
+            return packets;
+        }
     }
 }
