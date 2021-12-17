4f843a3082ed05549c4eaadf3f3e6a2062778984
Bart Broekman <ROB6035@robeco.nl>
2021-12-17 08:42:03 +0100

    Add day 18 and cleanup 17

--- AdventOfCode/Day17.cs
+++ AdventOfCode/Day17.cs
@@ -9,12 +9,58 @@ namespace AdventOfCode
     {
         public static void Part1()
         {
+            string[] input = Console.ReadLine()[15..].Split(", y=");
+            int[] xRange = input[0].Split("..").Select(s => int.Parse(s)).ToArray();
+            int[] yRange = input[1].Split("..").Select(s => int.Parse(s)).ToArray();
+            int minX = xRange[0], maxX = xRange[1], minY = yRange[0], maxY = yRange[1];
+            int highest = 0;
+            for (int i = 1; i <= maxX; i++)
+            {
+                for (int j = minY; j < 100; j++)
+                {
+                    int maxHeight = int.MinValue;
+                    for (int x = 0, y = 0, xVel = i, yVel = j; ; x += xVel, y += yVel, xVel = xVel > 0 ? xVel - 1 : (xVel < 0 ? xVel + 1 : xVel), yVel--)
+                    {
+                        if (y > maxHeight)
+                            maxHeight = y;
+                        if (x > maxX || (xVel == 0 && x < minX) || (yVel < 0 && y < minY))
+                            break;
 
+                        if (x >= minX && x <= maxX && y >= minY && y <= maxY)
+                        {
+                            if (maxHeight > highest)
+                                highest = maxHeight;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(highest);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
+                    string[] input = Console.ReadLine()[15..].Split(", y=");
+            int[] xRange = input[0].Split("..").Select(s => int.Parse(s)).ToArray();
+            int[] yRange = input[1].Split("..").Select(s => int.Parse(s)).ToArray();
+            int minX = xRange[0], maxX = xRange[1], minY = yRange[0], maxY = yRange[1];
+            int hits = 0;
+            for (int i = 0; i <= maxX; i++)
+                for (int j = minY; j < 10000; j++)
+                {
+                    for (int x = 0, y = 0, xVel = i, yVel = j; ; x += xVel, y += yVel, xVel = xVel > 0 ? xVel - 1 : (xVel < 0 ? xVel + 1 : xVel), yVel--)
+                    {
+                        if (x > maxX || (xVel == 0 && x < minX) || (yVel < 0 && y < minY))
+                            break;
 
-        }*/
+                        if (x >= minX && x <= maxX && y >= minY && y <= maxY)
+                        {
+                            hits++;
+                            break;
+                        }
+                    }
+                }
+            Console.WriteLine(hits);
+        }
     }
 }
