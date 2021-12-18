3f1e6b54a13e9d2ff657a21b070c905b31cf2f62
Bart Broekman <broekman@live.nl>
2021-12-18 17:31:23 +0100

    Add day 19 file

---
+++ AdventOfCode/Day19.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day19
+    {
+        public static void Part1()
+        {
+
+        }
+
+        /*public static void Part2()
+        {
+
+        }*/
+    }
+}
