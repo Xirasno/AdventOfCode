dbc07d366d9f9f03e4999019d551a3b858b67eda
Bart Broekman <broekman@live.nl>
2021-12-18 17:30:07 +0100

    Day18

--- AdventOfCode/Day18.cs
+++ AdventOfCode/Day18.cs
@@ -10,23 +10,67 @@ namespace AdventOfCode
         public static void Part1()
         {
             string input;
+            SnailNumber snailNr = new SnailNumber(Console.ReadLine()[1..^1], null);
             while ((input = Console.ReadLine()) != "")
             {
-                SnailNumber sn = new SnailNumber(input[1..^1], 1, null);
+                snailNr = new SnailNumber(snailNr, new SnailNumber(input[1..^1], null));
+                //Console.WriteLine("-----------------START-----------------"); Console.WriteLine(snailNr.Write()); Console.WriteLine("----------------REDUCED-----------------");
+
                 bool reduced = false;
                 while (!reduced)
                 {
-                    sn.Reduce();
+                    snailNr.FixDepth();
+                    reduced = snailNr.ReduceExplode();
+                    if (!reduced)
+                        continue;
+                    reduced = snailNr.ReduceSplit();
                 }
-                Console.WriteLine(sn.ToString());
+                //Console.WriteLine(snailNr.Write());
+                //Console.WriteLine("-----------------END------------------");
             }
-
+            Console.WriteLine(snailNr.Magnitude());
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
+            string input;
+            List<SnailNumber> numbers = new();
+            while ((input = Console.ReadLine()) != "")
+            {
+                numbers.Add(new SnailNumber(input[1..^1], null));
+            }
 
-        }*/
+            List<long> magnitudes = new();
+            foreach (SnailNumber number1 in numbers)
+            {
+                //Console.WriteLine("-----------------NR.01-----------------"); 
+                //Console.WriteLine(number1.Write());
+                foreach (SnailNumber number2 in numbers)
+                {
+                    if (number1 == number2)
+                        continue;
+                    SnailNumber snailNr = new SnailNumber(new SnailNumber(number1.Write()[1..^1], null), new SnailNumber(number2.Write()[1..^1], null));
+                    bool reduced = false;
+                    while (!reduced)
+                    {
+                        snailNr.FixDepth();
+                        reduced = snailNr.ReduceExplode();
+                        if (!reduced)
+                            continue;
+                        reduced = snailNr.ReduceSplit();
+                    }
+                    long magnitude = snailNr.Magnitude();
+                    /*Console.WriteLine("-----------------NR.02-----------------"); 
+                    Console.WriteLine(number2.Write());
+                    Console.WriteLine("----------------REDUCED-----------------");
+                    Console.WriteLine(snailNr.Write());
+                    Console.WriteLine("---------------MAGNITUDE----------------");
+                    Console.WriteLine(magnitude);*/
+                    magnitudes.Add(magnitude);
+                }
+            }
+            Console.WriteLine(magnitudes.Max());
+        }
     }
 
     public class SnailNumber
@@ -35,21 +79,28 @@ namespace AdventOfCode
         int value = -1;
         int depth;
 
-        public SnailNumber(string s, int depth, SnailNumber parent)
+        public SnailNumber(SnailNumber x, SnailNumber y)
         {
-            this.depth = depth;
+            x.parent = this;
+            y.parent = this;
+            this.x = x;
+            this.y = y;
+        }
+
+        public SnailNumber(string s, SnailNumber parent)
+        {
             this.parent = parent;
+            this.depth = this.parent != null ? parent.depth : 0;
             if (s.Length == 1)
             {
-                this.depth--;
                 this.value = int.Parse(s);
                 return;
             }
 
             if (s.Length == 3)
             {
-                this.x = new SnailNumber(s[0].ToString(), depth, this);
-                this.y = new SnailNumber(s[2].ToString(), depth, this);
+                this.x = new SnailNumber(s[0].ToString(), this);
+                this.y = new SnailNumber(s[2].ToString(), this);
                 return;
             }
 
@@ -82,64 +133,122 @@ namespace AdventOfCode
             newS1 = newS1.Length > 1 ? newS1[1..^1] : newS1;
             newS2 = newS2.Length > 1 ? newS2[1..^1] : newS2;
 
-            this.x = new SnailNumber(newS1, depth + 1, this);
-            this.y = new SnailNumber(newS2, depth + 1, this);
+            this.x = new SnailNumber(newS1, this);
+            this.y = new SnailNumber(newS2, this);
            
         }
 
-        public SnailNumber(int value, int depth, SnailNumber parent)
+        public SnailNumber(int value, SnailNumber parent)
         {
             this.value = value;
-            this.depth = depth;
             this.parent = parent;
+            this.depth = this.parent != null ? parent.depth : 0;
         }
 
-        public bool Reduce()
+        public void FixDepth()
+        {
+            if (this.parent == null)
+                this.depth = 0;
+            else
+                this.depth = parent.depth + 1;
+
+            if (x != null) x.FixDepth();
+            if (y != null) y.FixDepth();
+        }
+
+        public bool ReduceExplode()
         {
             if (this.x != null && this.y != null)
             {
-                if (depth == 4)
+                if (depth >= 4 && this.x.value != -1 && this.y.value != -1)
                 {
-                    parent.Explode(this);
+                    parent = parent.Explode(this);
+                    this.x = null;
+                    this.y = null;
+                    this.value = 0;
                     return false;
                 }
                 else
                 {
-                    this.x.Reduce();
-                    this.y.Reduce();
-                    return false;
+                    bool reduced = this.x.ReduceExplode();
+                    if (!reduced)
+                        return false;
+                    return this.y.ReduceExplode();
                 }
             }
-            else if (value >= 10)
+            return true;
+        }
+
+        public bool ReduceSplit()
+        {
+            if (this.x != null && this.y != null)
             {
-                this.x = new SnailNumber(value / 2, depth, this);
-                this.y = new SnailNumber(value % 2 == 0 ? value / 2 : (value / 2) + 1, depth, this);
+                bool reduced = this.x.ReduceSplit();
+                if (!reduced)
+                    return false;
+                return this.y.ReduceSplit();
+            }
+                if (value >= 10)
+            {
+                this.x = new SnailNumber(value / 2, this);
+                this.y = new SnailNumber(value % 2 == 0 ? value / 2 : (value / 2) + 1, this);
+                value = -1;
                 return false;
             }
             return true;
         }
 
-        public void Explode(SnailNumber s)
+        public SnailNumber Explode(SnailNumber s)
         {
             if (s == x)
             {
-                y.Add(s.y.value, 0);
-                try { this.parent.parent.x.Add(s.x.value, 1); } catch { }
+                y.Add(this.x.y.value, s, 0);
+                if (this.parent.x != null)
+                    this.parent.x.Add(s.x.value, s, 1);
+                x = new SnailNumber(0, this);
             }
             else if (s == y)
             {
-                x.Add(s.x.value, 1);
-                try { this.parent.parent.y.Add(s.x.value, 1); } catch { }
+                x.Add(this.y.x.value, s, 1);
+                if (this.parent.y != null)
+                    this.parent.y.Add(s.y.value, s, 0);
+            }
+            return this;
+        }
+
+        public void Add(int v, SnailNumber s, int lr)
+        {
+            if (x == null && y == null)
+            {
+                value += v;
+            }
+            else if (lr == 1)
+            {
+                if (this.x == s)
+                    try { this.parent.parent.x.Add(v, this, lr); } catch { }
+                else y.Add(v, this, 1);
+            }
+            else
+            {
+                if (this.y == s)
+                    try { this.parent.parent.y.Add(v, this, lr); } catch { }
+                else x.Add(v, this, 0);
             }
         }
 
-        public void Add(int v, int lr)
+        public long Magnitude()
         {
             if (x == null && y == null)
-                value += v;
-            else if (lr == 0)
-             x.Add(v, lr);
-            else y.Add(v, lr);
+                return value;
+            else 
+                return (3 * x.Magnitude()) + (2 * y.Magnitude());
+        }
+
+        public string Write()
+        {
+            if (x != null && y != null)
+                return $"[{x.Write()},{y.Write()}]";
+            else return this.value.ToString();
         }
     }
 }
