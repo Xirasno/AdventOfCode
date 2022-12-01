36c4b49987d5e2fd74cfb582996282d163988507
Bart Broekman <broekman@live.nl>
2022-12-01 08:10:50 +0100

    Day 1 AoC2022

--- AdventOfCode/AoC2022/Day01.cs
+++ AdventOfCode/AoC2022/Day01.cs
@@ -9,12 +9,41 @@ namespace AdventOfCode
     {
         public static void Part1()
         {
+            List<int> calories = new();
+            string input = Console.ReadLine();
+            int calorie = 0;
+            do
+            {
+                do
+                    calorie += Convert.ToInt32(input);
+                while ((input = Console.ReadLine()) != "");
 
+                calories.Add(calorie);
+                calorie = 0;
+
+            }
+            while ((input = Console.ReadLine()) != "");
+            Console.WriteLine(calories.Max());
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
+            List<int> calories = new();
+            string input = Console.ReadLine();
+            int calorie = 0;
+            do
+            {
+                do
+                    calorie += Convert.ToInt32(input);
+                while ((input = Console.ReadLine()) != "");
 
-        }*/
+                calories.Add(calorie);
+                calorie = 0;
+
+            }
+            while ((input = Console.ReadLine()) != "");
+            var newCalories = calories.OrderByDescending(n => n).ToArray();
+            Console.WriteLine(newCalories[0]+ newCalories[1]+ newCalories[2]);
+        }
     }
 }
