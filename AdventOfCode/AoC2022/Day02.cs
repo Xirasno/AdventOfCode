24ca96b0b5a5ccdbe246b861a0d6cee711e21088
Bart Broekman <broekman@live.nl>
2022-12-01 10:48:18 +0100

    add day02 file

---
+++ AdventOfCode/AoC2022/Day02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day2
+    {
+        public static void Part1()
+        {
+
+        }
+
+        /*public static void Part2()
+        {
+
+        }*/
+    }
+}
