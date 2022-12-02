1d26da114b3dffcd33abb9924a2049adad49cd55
Bart Broekman <broekman@live.nl>
2022-12-02 08:23:47 +0100

    add day 3

---
+++ AdventOfCode/AoC2022/Day03.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day3
+    {
+        public static void Part1()
+        {
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
