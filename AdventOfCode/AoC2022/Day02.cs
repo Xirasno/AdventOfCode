b7bb126aa058ec6b40ecb3c6e392cc2a160dee79
Bart Broekman <broekman@live.nl>
2022-12-03 08:59:39 +0100

    Remove ugly if, replace with switch

--- AdventOfCode/AoC2022/Day02.cs
+++ AdventOfCode/AoC2022/Day02.cs
@@ -29,64 +29,82 @@ namespace AdventOfCode
 
         private static int RPS(char X, char Y)
         {
-            if (X == 'X')
+            switch (X)
             {
-                if (Y == 'A')
-                    return 4;
-                if (Y == 'B')
-                    return 1;
-                if (Y == 'C')
-                    return 7;
-            }
-            if (X == 'Y')
-            {
-                if (Y == 'A')
-                    return 8;
-                if (Y == 'B')
-                    return 5;
-                if (Y == 'C')
-                    return 2;
-            }
-            if (X == 'Z')
-            {
-                if (Y == 'A')
-                    return 3;
-                if (Y == 'B')
-                    return 9;
-                if (Y == 'C')
-                    return 6;
+                case 'X':
+                    switch (Y)
+                    {
+                        case 'A':
+                            return 4;
+                        case 'B':
+                            return 1;
+                        case 'C':
+                            return 7;
+                    }
+                    break;
+                case 'Y':
+                    switch (Y)
+                    {
+                        case 'A':
+                            return 8;
+                        case 'B':
+                            return 5;
+                        case 'C':
+                            return 2;
+                    }
+                    break;
+                case 'Z':
+                    switch (Y)
+                    {
+                        case 'A':
+                            return 3;
+                        case 'B':
+                            return 9;
+                        case 'C':
+                            return 6;
+                    }
+                    break;
             }
             return -1;
         }
 
         private static int RPS2(char X, char Y)
         {
-            if (X == 'X')
+            switch (X)
             {
-                if (Y == 'A')
-                    return 3;
-                if (Y == 'B')
-                    return 1;
-                if (Y == 'C')
-                    return 2;
-            }
-            if (X == 'Y')
-            {
-                if (Y == 'A')
-                    return 4;
-                if (Y == 'B')
-                    return 5;
-                if (Y == 'C')
-                    return 6;
-            }
-            if (X == 'Z')
-            {
-                if (Y == 'A')
-                    return 8;
-                if (Y == 'B')
-                    return 9;
-                if (Y == 'C')
-                    return 7;
+                case 'X':
+                    switch (Y)
+                    {
+                        case 'A':
+                            return 3;
+                        case 'B':
+                            return 1;
+                        case 'C':
+                            return 2;
+                    }
+                    break;
+                case 'Y':
+                    switch (Y)
+                    {
+                        case 'A':
+                            return 4;
+                        case 'B':
+                            return 5;
+                        case 'C':
+                            return 6;
+                    }
+                    break;
+                case 'Z':
+                    switch (Y)
+                    {
+                        case 'A':
+                            return 8;
+                        case 'B':
+                            return 9;
+                        case 'C':
+                            return 7;
+                    }
+                    break;
             }
             return -1;
         }
