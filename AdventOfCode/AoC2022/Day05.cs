4f5345bee4593f7aff0727d36f30c27e96ad442e
Bart Broekman <broekman@live.nl>
2022-12-04 08:42:14 +0100

    Add day5 file

---
+++ AdventOfCode/AoC2022/Day05.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day5
+    {
+        public static void Part1()
+        {
+            string input;
+            Console.WriteLine();
+        }
+
+        /*public static void Part2()
+        {
+
+        }*/
+    }
+}
