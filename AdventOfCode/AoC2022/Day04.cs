367d53b7cfd0975aa906297bd051c0fecae6dd2e
Bart Broekman <broekman@live.nl>
2022-12-04 08:41:25 +0100

    Complete day 4

--- AdventOfCode/AoC2022/Day04.cs
+++ AdventOfCode/AoC2022/Day04.cs
@@ -10,12 +10,34 @@ namespace AdventOfCode
         public static void Part1()
         {
             string input;
-            Console.WriteLine();
+            int elf1S, elf1E, elf2S, elf2E, overlap = 0;
+            while ((input = Console.ReadLine()) != "")
+            {
+                elf1S = int.Parse(input.Split(',')[0].Split('-')[0]);
+                elf1E = int.Parse(input.Split(',')[0].Split('-')[1]);
+                elf2S = int.Parse(input.Split(',')[1].Split('-')[0]);
+                elf2E = int.Parse(input.Split(',')[1].Split('-')[1]);
+                if ((elf2S >= elf1S && elf2E <= elf1E) || (elf1S >= elf2S && elf1E <= elf2E))
+                        overlap++;
+            }
+            Console.WriteLine(overlap);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
+            string input;
+            int elf1S, elf1E, elf2S, elf2E, overlap = 0;
+            while ((input = Console.ReadLine()) != "")
+            {
+                elf1S = int.Parse(input.Split(',')[0].Split('-')[0]);
+                elf1E = int.Parse(input.Split(',')[0].Split('-')[1]);
+                elf2S = int.Parse(input.Split(',')[1].Split('-')[0]);
+                elf2E = int.Parse(input.Split(',')[1].Split('-')[1]);
+                if ((elf2S >= elf1S && elf2S <= elf1E) || (elf2E <= elf1E && elf2E >= elf1S) || (elf1S >= elf2S && elf1S <= elf2E) || (elf1E <= elf2E && elf1E >= elf2S))
+                    overlap++;
+            }
+            Console.WriteLine(overlap);
 
-        }*/
+        }
     }
 }
