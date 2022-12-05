f435bff1371d257db031f61b1f51946de90cc118
Bart Broekman <broekman@live.nl>
2022-12-05 08:47:42 +0100

    Complete day5

--- AdventOfCode/AoC2022/Day05.cs
+++ AdventOfCode/AoC2022/Day05.cs
@@ -7,15 +7,66 @@ namespace AdventOfCode
 {
     public static class AoC2022Day5
     {
+        private readonly static string[] stacks = new string[9] {
+                "NSDCVQT",
+                "MFV",
+                "FQWDPNHM",
+                "DQRTF",
+                "RFMNQHVB",
+                "CFGNPWQ",
+                "WFRLCT",
+                "TZNS",
+                "MSDJRQHN" };
+
+        // TestInput
+        /*private readonly static string[] stacks = new string[3] {
+            "ZN",
+            "MCD",
+            "P" };*/
+
         public static void Part1()
         {
             string input;
-            Console.WriteLine();
+            while ((input = Console.ReadLine()) != "")
+            {
+                int amnt = int.Parse(input.Split(' ')[1]);
+                int from = int.Parse(input.Split(' ')[3]) - 1;
+                int to   = int.Parse(input.Split(' ')[5]) - 1;
+                for (int i = 0; i < amnt; i++)
+                {
+                    stacks[to] += stacks[from][^1];
+                    stacks[from] = stacks[from].Remove(stacks[from].Length - 1);
+                }
+            }
+            foreach(var c in stacks)
+            {
+                Console.Write(c.Last());
+            }
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
-
-        }*/
+            string input, temp = "";
+            while ((input = Console.ReadLine()) != "")
+            {
+                int amnt = int.Parse(input.Split(' ')[1]);
+                int from = int.Parse(input.Split(' ')[3]) - 1;
+                int to   = int.Parse(input.Split(' ')[5]) - 1;
+                for (int i = 0; i < amnt; i++)
+                {
+                    temp += stacks[from][^1];
+                    stacks[from] = stacks[from].Remove(stacks[from].Length - 1);
+                }
+                for (int i = 0; i < amnt; i++)
+                {
+                    stacks[to] += temp[^1];
+                    temp = temp.Remove(temp.Length - 1);
+                }
+            }
+            foreach(var c in stacks)
+            {
+                Console.Write(c.Last());
+            }
+        }
     }
 }
