ebc159f0eb36ce78b8bf3c5edf8f57a77615abeb
Bart Broekman <broekman@live.nl>
2022-12-06 08:14:01 +0100

    Complete day 6

--- AdventOfCode/AoC2022/Day06.cs
+++ AdventOfCode/AoC2022/Day06.cs
@@ -9,15 +9,42 @@ namespace AdventOfCode
     {
         public static void Part1()
         {
-            string input;
-            while ((input = Console.ReadLine()) != "")
+            string input = Console.ReadLine(), buffer = "";
+            for(int i = 0; i < input.Length; i++)
             {
+                if (!buffer.Contains(input[i]))
+                    buffer += input[i];
+                else
+                {
+                    buffer = buffer.Replace(input[i], ' ');
+                    buffer = buffer.Split(' ')[1] + input[i];
+                }
+                if (buffer.Length == 4)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
             }
-            Console.WriteLine();
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
-        }*/
+            string input = Console.ReadLine(), buffer = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!buffer.Contains(input[i]))
+                    buffer += input[i];
+                else
+                {
+                    buffer = buffer.Replace(input[i], ' ');
+                    buffer = buffer.Split(' ')[1] + input[i];
+                }
+                if (buffer.Length == 14)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+        }
     }
 }
