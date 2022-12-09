84170d104654b8a5a2ad6bb0dc1758bafe7b38a2
Bart Broekman <broekman@live.nl>
2022-12-09 10:53:13 +0100

    Add day 10

---
+++ AdventOfCode/AoC2022/Day10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day10
+    {
+        public static void Part1()
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "")
+            {
+            }
+            Console.WriteLine();
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
