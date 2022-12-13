3c625eb049b77003a534792f750258138ae90a6e
Bart Broekman <broekman@live.nl>
2022-12-13 13:40:24 +0100

    Add day 14

---
+++ AdventOfCode/AoC2022/Day14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day14
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+            }
+            Console.WriteLine();
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
