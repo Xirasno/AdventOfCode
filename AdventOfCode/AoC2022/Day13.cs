bc8871d875c316107e460498861441cd153354be
Bart Broekman <broekman@live.nl>
2022-12-13 13:27:02 +0100

    Complete Day13

--- AdventOfCode/AoC2022/Day13.cs
+++ AdventOfCode/AoC2022/Day13.cs
@@ -10,28 +10,69 @@ namespace AdventOfCode
         public static void Part1()
         {
             string input;
-            List<int> rightOrder = new();
+            int rightOrder = 0;
             int index = 0;
             while ((input = Console.ReadLine()) != "")
             {
                 index++;
                 var left = ParsePacket(input);
                 var right = ParsePacket(Console.ReadLine());
-                if (ComparePackets(left, right) == 1)
-                    rightOrder.Add(index);
+                Console.WriteLine($"Pair: {index}");
+                var p = ComparePackets(left, right);
+                Console.WriteLine($"Result: {p}");
+                if (p == 1)
+                    rightOrder += index;
 
                 Console.ReadLine();
             }
-            Console.WriteLine(rightOrder.Sum());
+            Console.WriteLine(rightOrder++);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
-        }*/
+            string input;
+            List<Packet> packets = new();
+
+            Packet p2 = new Packet(-1, null)
+            { packets = new List<Packet>() { new Packet(2, null) } };
+            Packet p6 = new Packet(-1, null)
+            { packets = new List<Packet>() { new Packet(6, null) } };
+            packets.Add(p2);
+            packets.Add(p6);
+
+            int index = 0;
+            while ((input = Console.ReadLine()) != "")
+            {
+                packets.Add(ParsePacket(input));
+                packets.Add(ParsePacket(Console.ReadLine()));
+                Console.ReadLine();
+            }
+
+            for (int i = 0; i < packets.Count - 1; i++)
+            {
+                for (int j = 0; j < packets.Count - i - 1; j++)
+                {
+                    var p = ComparePackets(packets[j], packets[j + 1]);
+                    if (p == -1)
+                    {
+                        var temp = packets[j];
+                        var temp2 = packets[j + 1];
+
+                        packets[j] = temp2;
+                        packets[j + 1] = temp;
+                    }
+                }
+            }
+            int i2 = packets.IndexOf(packets.First(p => p.packets.Any(l => l.value == 2 && l.parent == null))) + 1;
+            int i6 = packets.IndexOf(packets.First(p => p.packets.Any(l => l.value == 6 && l.parent == null))) + 1;
+            Console.WriteLine(i2*i6);
+        }
 
         // 1 = true, 0 = tbd, -1 = false;
         public static int ComparePackets(Packet left, Packet right)
         {
+            /*Console.WriteLine($"========================================================================================\n" +
+                $"Left: {left.value}, Right: {right.value}\n");*/
             if (left.packets.Count == 0 && right.packets.Count == 0)
             {
                 if (left.value == -1 && right.value != -1)
@@ -39,7 +80,7 @@ namespace AdventOfCode
                 if (left.value < right.value)
                     return 1;
                 else if (left.value == right.value)
-                    return 0; // not really true, but not false either
+                    return 0;
                 return -1;
             }
 
@@ -105,7 +146,9 @@ namespace AdventOfCode
                     case ',':
                         break;
                     default:
-                        curPacket.packets.Add(new Packet(int.Parse(input[i].ToString()), curPacket));
+                        if(int.TryParse(input[i+1].ToString(), out var k))
+                            curPacket.packets.Add(new Packet(int.Parse(input[i..(i+2)].ToString()), curPacket));
+                        else curPacket.packets.Add(new Packet(int.Parse(input[i].ToString()), curPacket));
                         break;
                 }
             }
