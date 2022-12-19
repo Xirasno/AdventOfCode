8a3686bc384fbb38dcbd42661a886e0833031a98
Bart Broekman <broekman@live.nl>
2022-12-19 07:29:44 +0100

    Add Day 20

---
+++ AdventOfCode/AoC2022/Day20.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day20
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+                var split = input.Split(' ');
+            }
+
+            Console.WriteLine();
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
