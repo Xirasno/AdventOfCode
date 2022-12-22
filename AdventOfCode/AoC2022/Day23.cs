fe3f7f2249970e58ece6e7ba3029e25fa367c8c0
Bart Broekman <broekman@live.nl>
2022-12-22 14:00:04 +0100

    Add day23

---
+++ AdventOfCode/AoC2022/Day23.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day23
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
