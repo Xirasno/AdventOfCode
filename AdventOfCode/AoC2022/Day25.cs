e6cd9ab3b7898657f883f5e1c4c9a4db9511ccd8
Bart Broekman <broekman@live.nl>
2022-12-24 08:30:32 +0100

    Add day 25

---
+++ AdventOfCode/AoC2022/Day25.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AdventOfCode
+{
+    public static class AoC2022Day25
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+                var split = input.Split(' ');
+
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
