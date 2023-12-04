625234376ecb98e48cb39caa4e92246e55a66e96
Bart Broekman <broekman@live.nl>
2023-12-04 08:31:32 +0100

    Complete Day 4

--- AdventOfCode/AoC2023/Day04.cs
+++ AdventOfCode/AoC2023/Day04.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AdventOfCode
 {
@@ -11,15 +12,76 @@ namespace AdventOfCode
         public static void Part1()
         {
             string input;
+            List<int> myNumbers = new(), winningNumbers = new();
+            Regex nrs = new(@"\d+");
+            int sum = 0;
             while ((input = Console.ReadLine()) != "")
             {
+                int score = 0;
+                string[] splitInput = input.Split('|');
+                winningNumbers = nrs.Matches(splitInput[1]).Select(m => int.Parse(m.Value)).ToList();
+                myNumbers = nrs.Matches(splitInput[0].Split(": ")[1]).Select(m => int.Parse(m.Value)).ToList();
+
+                myNumbers.ForEach(n =>
+                {
+                    if (winningNumbers.Contains(n))
+                    {
+                        if (score == 0)
+                            score = 1;
+                        else 
+                        score <<= 1;
+                    }
+                });
+                sum += score;
             }
 
-            Console.WriteLine();
+            Console.WriteLine(sum);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
-        }*/
+            string input;
+            List<int> myNumbers = new(), winningNumbers = new();
+            Regex nrs = new(@"\d+");
+            int sum = 0;
+            List<ScratchCard> scratchCards= new();
+            while ((input = Console.ReadLine()) != "")
+            {
+                scratchCards.Add(new(input, 1));
+            }
+            for (int i = 0; i < scratchCards.Count; i++)
+            {
+                int score = 0;
+                string[] splitInput = scratchCards[i].Card.Split('|');
+                winningNumbers = nrs.Matches(splitInput[1]).Select(m => int.Parse(m.Value)).ToList();
+                myNumbers = nrs.Matches(splitInput[0].Split(": ")[1]).Select(m => int.Parse(m.Value)).ToList();
+
+                myNumbers.ForEach(n =>
+                {
+                    if (winningNumbers.Contains(n))
+                    {
+                        score++;
+                    }
+                });
+                for (int n = 1; n <= score; n++)
+                {
+                    scratchCards[i + n] = new (scratchCards[i + n].Card, scratchCards[i + n].Count + scratchCards[i].Count);
+                }
+            }
+
+            Console.WriteLine(scratchCards.Sum(c => c.Count));
+        }
+
+        public struct ScratchCard
+        {
+            public string Card;
+            public int Count;
+
+            public ScratchCard(string card, int count)
+            {
+                Card = card;
+                Count = count;
+            }
+        }
     }
 }
