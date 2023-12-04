c96e2b17c79be16b0740f6423ea51150ab6e4913
Bart Broekman <broekman@live.nl>
2023-12-04 08:32:13 +0100

    Add day 5

---
+++ AdventOfCode/AoC2023/Day05.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class AoC2023Day05
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+
+            }
+            Console.WriteLine(input);
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
