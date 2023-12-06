0607f72261c1439cd15acf3da85e0f1fec4e760b
Bart Broekman <broekman@live.nl>
2023-12-06 08:15:22 +0100

    Complete Day 6

--- AdventOfCode/AoC2023/Day06.cs
+++ AdventOfCode/AoC2023/Day06.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode
 {
@@ -6,17 +9,48 @@ namespace AdventOfCode
     {
         public static void Part1()
         {
-            string input;
-            while ((input = Console.ReadLine()) != "")
-            {
+            Regex nrs = new(@"\d+");
+            List<int> times = nrs.Matches(Console.ReadLine()).Select(m => int.Parse(m.Value)).ToList();
+            List<int> distances = nrs.Matches(Console.ReadLine()).Select(m => int.Parse(m.Value)).ToList();
 
+            int product = 1, possibleWays;
+            for (int i = 0; i < times.Count; i++)
+            {
+                possibleWays = 0;
+
+                for (int x = 0; x < times[i]; x++)
+                {
+                    if (x * (times[i] - x) > distances[i])
+                    {
+                        possibleWays++;
+                    }
+
+                }
+
+                product *= possibleWays;
             }
-            Console.WriteLine(input);
+
+            Console.WriteLine(product);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
+            Regex nrs = new(@"\d+");
+            long times = long.Parse(nrs.Matches(Console.ReadLine()).Select(m => m.Value).Aggregate((a, b) => a + b));
+            long distances = long.Parse(nrs.Matches(Console.ReadLine()).Select(m => m.Value).Aggregate((a, b) => a + b));
 
-        }*/
+            long possibleWays = 0;
+
+            for (long x = 0; x < times; x++)
+            {
+                if (x * (times - x) > distances)
+                {
+                    possibleWays++;
+                }
+
+            }
+
+            Console.WriteLine(possibleWays);
+        }
     }
 }
