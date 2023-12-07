70fc7150cc7bbd6da372337b301d468004af2bce
Bart Broekman <broekman@live.nl>
2023-12-07 09:11:13 +0100

    Add day 08

---
+++ AdventOfCode/AoC2023/Day08.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class AoC2023Day08
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+            }
+            Console.WriteLine();
+        }
+
+        /*public static void Part2()
+        {
+           
+        }*/
+    }
+}
