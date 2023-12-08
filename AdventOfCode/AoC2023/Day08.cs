2fe9715b20b7b03d3652568fb5977569a69f031b
Bart Broekman <broekman@live.nl>
2023-12-08 07:21:16 +0100

    Complete day 8

--- AdventOfCode/AoC2023/Day08.cs
+++ AdventOfCode/AoC2023/Day08.cs
@@ -9,16 +9,115 @@ namespace AdventOfCode
     {
         public static void Part1()
         {
+            string instructions =  Console.ReadLine();
+            Console.ReadLine();
+
+            Dictionary<string, Node> nodes = new();
+            Regex lettersRgx = new Regex(@"\w+");
             string input;
             while ((input = Console.ReadLine()) != "")
             {
+                List<string> letters = lettersRgx.Matches(input).Select(m => m.Value).ToList();
+                nodes.Add(letters[0], new(letters[1], letters[2]));
             }
-            Console.WriteLine();
+
+            string curNode = "AAA";
+            int steps = 0, moveIndex = 0;
+
+            while (curNode != "ZZZ")
+            {
+                switch (instructions[moveIndex])
+                {
+                    case 'R':
+                        curNode = nodes[curNode].Rigth;
+                        break;
+                    case 'L':
+                        curNode = nodes[curNode].Left;
+                        break;   
+                    default:
+                        break;
+                }
+                steps++;
+                moveIndex = moveIndex == instructions.Length - 1 ? 0 : moveIndex + 1;
+            }
+            Console.WriteLine(steps);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
-           
-        }*/
+            string instructions = Console.ReadLine();
+            Console.ReadLine();
+
+            Dictionary<string, Node> nodes = new();
+            Regex lettersRgx = new Regex(@"\w+");
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+                List<string> letters = lettersRgx.Matches(input).Select(m => m.Value).ToList();
+                nodes.Add(letters[0], new(letters[1], letters[2]));
+            }
+
+            List<string> curNodes = nodes.Where(n => n.Key[^1] == 'A').Select(n => n.Key).ToList();
+            List<long> neededSteps = new();
+
+            for (int i = 0; i < curNodes.Count; i++)
+            {
+                long steps = 0;
+                int moveIndex = 0;
+                while (curNodes[i][^1] != 'Z')
+                {
+                    switch (instructions[moveIndex])
+                    {
+                        case 'R':
+                            curNodes[i] = nodes[curNodes[i]].Rigth;
+                            break;
+                        case 'L':
+                            curNodes[i] = nodes[curNodes[i]].Left;
+                            break;
+                        default:
+                            break;
+                    }
+                    steps++;
+                    moveIndex = moveIndex == instructions.Length - 1 ? 0 : moveIndex + 1;
+                }
+                neededSteps.Add(steps);
+            }
+            Console.WriteLine(neededSteps.Aggregate((a, b) => FindLCM(a, b)));
+        }
+
+        public static long FindLCM(long a, long b)
+        {
+            long num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (long i = 1; i < num2; i++)
+            {
+                long mult = num1 * i;
+                if (mult % num2 == 0)
+                {
+                    return mult;
+                }
+            }
+            return num1 * num2;
+        }
+    }
+
+    public struct Node
+    {
+        public string Left;
+        public string Rigth;
+
+        public Node(string Left, string Right)
+        {
+            this.Left = Left;
+            this.Rigth = Right;
+        }
     }
 }
