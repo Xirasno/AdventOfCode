21e8c3c174324c0844ef752761b06658272d1293
Bart Broekman <broekman@live.nl>
2023-12-08 07:22:09 +0100

    Add day 9

---
+++ AdventOfCode/AoC2023/Day09.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public static class AoC2023Day09
+    {
+        public static void Part1()
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+            }
+            Console.WriteLine();
+        }
+
+        /*public static void Part2()
+        {
+        }*/
+    }
+}
