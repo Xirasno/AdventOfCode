f5ac16888d1841732c4c151f9d8f57e9413b6dbc
Bart Broekman <broekman@live.nl>
2023-12-09 10:48:19 +0100

    Complete Day 9

--- AdventOfCode/AoC2023/Day09.cs
+++ AdventOfCode/AoC2023/Day09.cs
@@ -9,15 +9,84 @@ namespace AdventOfCode
     {
         public static void Part1()
         {
+            List<List<int>> sequences = new();
             string input;
             while ((input = Console.ReadLine()) != "")
             {
+                sequences.Add(input.Split(' ').Select(m => int.Parse(m)).ToList());
             }
-            Console.WriteLine();
+
+            long sum = 0, n = 0;
+            foreach (var sequence in sequences)
+            {
+                List<int> sequenceSums = new() { sequence[^1] };
+                List<int> sequenceSum = new(sequence), newSequenceSum = new(sequence);
+
+                while (newSequenceSum.Any(m => m != 0))
+                {
+                    sequenceSum = new(newSequenceSum);
+                    newSequenceSum.Clear();
+
+                    if (sequenceSum.Count == 1)
+                    {
+                        newSequenceSum.Add(0);
+                    }
+
+                    for (int i = 0; i < sequenceSum.Count - 1; i++)
+                    {
+                        newSequenceSum.Add(sequenceSum[i + 1] - sequenceSum[i]);
+                    }
+                    sequenceSums.Add(newSequenceSum[^1]);
+                }
+
+                sum += sequenceSums.Sum();
+                n++;
+            }
+
+            Console.WriteLine(sum);
         }
 
-        /*public static void Part2()
+        public static void Part2()
         {
-        }*/
+            List<List<int>> sequences = new();
+            string input;
+            while ((input = Console.ReadLine()) != "")
+            {
+                sequences.Add(input.Split(' ').Select(m => int.Parse(m)).ToList());
+            }
+
+            long sum = 0, n = 0;
+            foreach (var sequence in sequences)
+            {
+                List<int> sequenceSums = new() { sequence[0] };
+                List<int> sequenceSum = new(sequence), newSequenceSum = new(sequence);
+
+                while (newSequenceSum.Any(m => m != 0))
+                {
+                    sequenceSum = new(newSequenceSum);
+                    newSequenceSum.Clear();
+
+                    if (sequenceSum.Count == 1)
+                    {
+                        newSequenceSum.Add(0);
+                    }
+
+                    for (int i = 0; i < sequenceSum.Count - 1; i++)
+                    {
+                        newSequenceSum.Add(sequenceSum[i + 1] - sequenceSum[i]);
+                    }
+                    sequenceSums.Add(newSequenceSum[0]);
+                }
+
+                long prev = 0;
+                for (int i = sequenceSums.Count - 1; i > 0; i--)
+                {
+                    prev = sequenceSums[i - 1] - prev;
+                }
+                sum += prev;
+            }
+
+            Console.WriteLine(sum);
+        }
     }
 }
